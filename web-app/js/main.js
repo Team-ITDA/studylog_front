7289f8d851e72c7f9f4ebeaf6fd89423710354f1
unknown <yangjinmin95@gmail.com>
2020-11-24 16:38:28 +0900

    header, aside, main css파일 분리 / aside 버튼 클릭 기능 / aside 리스트 길어질때 스크롤 기능 / script 파일 수정

--- web-app/js/main.js
+++ web-app/js/main.js
@@ -1,40 +1,61 @@
-function asideButton(){
-    // aside 메뉴 버튼 클릭 기능
+function registerAsideButtonClickEvent(){
+    // asidebutton을 클릭했을 때 aside 보여주고 숨기는 기능
+    const asideContainer = document.querySelector("#aside");
+    const asideContainerButton = aside.querySelector(".fa-angle-double-left");
+    const mainContainer = document.querySelector("#main");
 
-    const aside = document.querySelector("#aside");
-    const button = aside.querySelector(".fa-angle-double-left");
-
-    button.addEventListener("click", function (){
-        if(this.classList.contains("show")){
-            aside.classList.add("hide");
-            this.classList.add("hide");
-            aside.classList.remove("show");
-            this.classList.remove("show");
+    asideContainerButton.addEventListener("click", function (){
+        if(asideContainerButton.classList.contains("show")){
+            asideContainer.classList.add("hide");
+            asideContainerButton.classList.add("hide");
+            asideContainer.classList.remove("show");
+            asideContainerButton.classList.remove("show");
+            mainContainer.classList.add("increase");
+            mainContainer.classList.remove("reduce");
         } else {
-            aside.classList.add("show");
-            this.classList.add("show");
-            aside.classList.remove("hide");
-            this.classList.remove("hide");
+            asideContainer.classList.add("show");
+            asideContainerButton.classList.add("show");
+            asideContainer.classList.remove("hide");
+            asideContainerButton.classList.remove("hide");
+            mainContainer.classList.add("reduce");
+            mainContainer.classList.remove("increase");
         }
     });
 }
 
-asideButton();
+registerAsideButtonClickEvent();
 
-function userInfoMouse(){
-    // 클릭으로 바꿀 예정
-    const userInfo = document.querySelector("#aside-userInfo");
+var isUserInfoShow = false;
 
-    userInfo.addEventListener("mouseover", function (){
-        const userInfoDetail = this.nextElementSibling;
-        userInfoDetail.classList.add("show");
-        userInfoDetail.classList.remove("hide");
+function registerUserInfoClickEvent(){
+    // userInfo를 클릭했을 때 userInfoDetails를 보여주는 기능
+    const userInfoArea = document.querySelector("#aside-userInfo");
 
-        userInfoDetail.addEventListener("mouseleave", function (){
-            this.classList.remove("show");
-            this.classList.add("hide");
-        });
+    userInfoArea.addEventListener("click", function(){
+        const userInfoDetail = userInfoArea.nextElementSibling;
+
+        if(!isUserInfoShow){
+            userInfoDetail.classList.add("show");
+            userInfoDetail.classList.remove("hide");
+        }else{
+            userInfoDetail.classList.add("hide");
+            userInfoDetail.classList.remove("show");
+        }
+
+        isUserInfoShow = !isUserInfoShow;
     });
 }
 
-userInfoMouse();
+registerUserInfoClickEvent();
+
+
+// function init(){
+//     main top값에 대한 제어 연구중
+//     const mainContainer = document.querySelector("#main");
+//     const headerHeight = document.getElementById("header");
+//     const h = headerHeight.clientHeight;
+//     console.log(h);
+//     mainContainer.style.marginTop = h;
+// }
+//
+// init();
