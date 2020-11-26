fa0c07825825c7817a38202e1b6d7fa3ab063e22
unknown <yangjinmin95@gmail.com>
2020-11-27 00:37:38 +0900

    html-login 모달창 생성(css 수정) / 로그인 영역에 대한 script 연구

--- web-app/js/main.js
+++ web-app/js/main.js
@@ -1,11 +1,11 @@
-function registerAsideButtonClickEvent(){
+function registerAsideButtonClickEvent() {
     // asidebutton을 클릭했을 때 aside 보여주고 숨기는 기능
     const asideContainer = document.querySelector("#aside");
     const asideContainerButton = aside.querySelector(".fa-angle-double-left");
     const mainContainer = document.querySelector("#main");
 
-    asideContainerButton.addEventListener("click", function (){
-        if(asideContainerButton.classList.contains("show")){
+    asideContainerButton.addEventListener("click", function () {
+        if (asideContainerButton.classList.contains("show")) {
             asideContainer.classList.add("hide");
             asideContainerButton.classList.add("hide");
             asideContainer.classList.remove("show");
@@ -25,34 +25,56 @@ function registerAsideButtonClickEvent(){
 
 registerAsideButtonClickEvent();
 
+var isUserLoggedIn = false;
 var isUserInfoShow = false;
 
-function registerUserInfoClickEvent(){
-    // userInfo를 클릭했을 때 userInfoDetails를 보여주는 기능
+function registerUserInfoClickEvent() {
     const userInfoArea = document.querySelector("#aside-userInfo");
+    if (isUserLoggedIn) {
+        // userInfo를 클릭했을 때 userInfoDetails를 보여주는 기능
+        userInfoArea.addEventListener("click", function () {
+            const userInfoDetail = userInfoArea.nextElementSibling;
 
-    userInfoArea.addEventListener("click", function(){
-        const userInfoDetail = userInfoArea.nextElementSibling;
+            if (!isUserInfoShow) {
+                userInfoDetail.classList.add("show");
+                userInfoDetail.classList.remove("hide");
+            } else {
+                userInfoDetail.classList.add("hide");
+                userInfoDetail.classList.remove("show");
+            }
 
-        if(!isUserInfoShow){
-            userInfoDetail.classList.add("show");
-            userInfoDetail.classList.remove("hide");
-        }else{
-            userInfoDetail.classList.add("hide");
-            userInfoDetail.classList.remove("show");
-        }
+            isUserInfoShow = !isUserInfoShow;
+        });
+    } else {
+        userInfoArea.addEventListener("click", function (){
+            const loginArea = document.querySelector("#login");
+            loginArea.style.display = "block";
 
-        isUserInfoShow = !isUserInfoShow;
-    });
+            const loginAreaCloseButton = loginArea.querySelector(".login-area-closeButton");
+            loginAreaCloseButton.addEventListener("click", function (){
+                loginArea.style.display = "none";
+            });
+
+            const loginButtons = loginArea.querySelectorAll(".login-area-button");
+            const userInfoAreaId = userInfoArea.querySelector("#aside-userInfo-id");
+            loginButtons.forEach(function (button){
+                button.addEventListener("click", function (){
+                   isUserLoggedIn = !isUserLoggedIn;
+                   loginArea.style.display = "none";
+                   userInfoAreaId.innerHTML = "Jinmin";
+                });
+            });
+        });
+    }
+
 }
 
 registerUserInfoClickEvent();
 
-
-function registerAsideNavTitleClickEvent(){
+function registerAsideNavTitleClickEvent() {
     const asideNavTitles = document.querySelectorAll(".aside-nav-title");
 
-    asideNavTitles.forEach(function (title){
+    asideNavTitles.forEach(function (title) {
         title.addEventListener("click", function () {
             const controledAsideNavList = title.nextElementSibling;
             const asideNavTitlesIcon = title.firstElementChild;
@@ -69,16 +91,6 @@ function registerAsideNavTitleClickEvent(){
             }
         });
     });
-/*    asideNavTitles.addEventListener("click", function (){
-        const controledAsideNavList = asideNavTitle.nextElementSibling;
-        if(controledAsideNavList.classList.contains("show")){
-           controledAsideNavList.classList.add("hide");
-           controledAsideNavList.classList.remove("show");
-        } else {
-            controledAsideNavList.classList.add("show");
-            controledAsideNavList.classList.remove("hide");
-        }
-    });*/
 }
 
 registerAsideNavTitleClickEvent();
@@ -91,7 +103,9 @@ registerAsideNavTitleClickEvent();
 
 
 
+function registerAsideLoginClickEvent() {
 
+}
 
 
 
