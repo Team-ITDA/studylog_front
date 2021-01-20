5de8db7f0d0d87c209036922697eb4789cc84ebb
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-01-20 19:11:54 +0900

    feat: 유저상태 모달 위치 변경
    - 사이드바에서 컨트롤했던 유저상태 모달 헤더에서 컨트롤 및 위치 변경

--- web-app/js/header.js
+++ web-app/js/header.js
@@ -1,32 +1,62 @@
 class Header {
+    #isUserContainerShow = false;
+
     #searchContainer = document.querySelector(".header-search");
     #loginContainer = document.querySelector(".login-container");
 
     constructor() {
+        this.#loginContainer.querySelector(".login-article").addEventListener("click", event => this.#login(event));
+
         document.querySelector(".header-search-input").addEventListener("focus", this.#focusSearchInput.bind(this));
         document.querySelector(".header-search-input").addEventListener("focusout", this.#focusoutSearchInput.bind(this));
         document.querySelector(".header-login-button").addEventListener("click", this.#showLoginContainer.bind(this));
         document.querySelector(".login-area-closeButton").addEventListener("click", this.#hideLoginContainer.bind(this));
+        document.querySelector(".header-user-info").addEventListener("click", this.#showOrHideUserContainer.bind(this));
+        document.querySelector(".logout").addEventListener("click", this.#logout.bind(this));
     }
 
     #focusSearchInput() {
         this.#searchContainer.classList.add("focus");
-        this.#searchContainer.classList.remove("focusout");
     }
 
     #focusoutSearchInput() {
-        this.#searchContainer.classList.add("focusout");
         this.#searchContainer.classList.remove("focus");
     }
 
     #showLoginContainer() {
-        this.#loginContainer.classList.add("show");
         this.#loginContainer.classList.remove("hide");
     }
 
     #hideLoginContainer() {
-        this.#loginContainer.classList.remove("show");
         this.#loginContainer.classList.add("hide");
+    }
+
+    #showOrHideUserContainer() {
+        if (this.#isUserContainerShow) {
+            document.querySelector(".user-container").classList.add("hide");
+            document.querySelector(".fa-angle-down").classList.remove("hide");
+            document.querySelector(".fa-angle-up").classList.add("hide");
+        } else {
+            document.querySelector(".user-container").classList.remove("hide");
+            document.querySelector(".fa-angle-down").classList.add("hide");
+            document.querySelector(".fa-angle-up").classList.remove("hide");
+        }
+        this.#isUserContainerShow = !this.#isUserContainerShow;
+    }
+
+    #login(event) {
+        if (event.target.classList.contains("login-button") ||
+            event.target.closest("div").classList.contains("login-button")) {
+            document.querySelector(".header-login-button").classList.add("hide");
+            document.querySelector(".header-user-info").classList.remove("hide");
+            this.#hideLoginContainer();
+        }
+    }
+
+    #logout() {
+        document.querySelector(".header-login-button").classList.remove("hide");
+        document.querySelector(".header-user-info").classList.add("hide");
+        this.#showOrHideUserContainer();
     }
 }
 
