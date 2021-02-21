0d39bea0cb021a97c4cbf09c31b13b1b4d9997f1
Jinmin <yangjinmin95@gmail.com>
2021-02-21 22:46:39 +0900

    feat: 글 종류 삭제, 해시태그 추가, open과 close에 대한 효과 수정

--- web-app/js/review_list.js
+++ web-app/js/review_list.js
@@ -1,16 +1,27 @@
 const openButton = document.querySelector('.open-button');
 const closeButton = document.querySelector('.close-button');
 const reviewItems = document.querySelector('.review-items');
-const createButton = document.querySelector('.create-button');
+const reviewListItems = reviewItems.querySelectorAll('.review-list-item');
+const searchBox = document.querySelector('.search-box');
+const searchInput = searchBox.querySelector('.search-input');
 
 openButton.addEventListener('click', function (event) {
     closeButton.classList.remove('checked');
     reviewItems.classList.remove('close-checked');
-    createButton.classList.remove('close-checked');
+    searchBox.classList.remove('close-checked');
+    searchInput.classList.remove('close-checked');
+    reviewListItems.forEach((element) => {
+        element.classList.remove('close-checked');
+    });
 });
 
 closeButton.addEventListener('click', function (event) {
     closeButton.classList.add('checked');
     reviewItems.classList.add('close-checked');
-    createButton.classList.add('close-checked');
+    searchBox.classList.add('close-checked');
+    searchInput.classList.add('close-checked');
+    reviewListItems.forEach((element) => {
+        element.classList.add('close-checked');
+    });
+
 });
