7524f887cdeb80e9a0cd7388c5ac67b6139e896d
Jinmin <yangjinmin95@gmail.com>
2021-02-23 19:45:08 +0900

    feat: 검색영역 추가, script 수정

--- web-app/js/post_list.js
+++ web-app/js/post_list.js
@@ -1,7 +1,7 @@
 const card = document.querySelectorAll('.card');
 
 card.forEach((element) => {
-    const isHaveThumbnail = element.children.item('card-image').children.item('.card-image-thumbnail');
+    const isHaveThumbnail = element.querySelector('.card-image-thumbnail');
     if (!isHaveThumbnail) {
         element.classList.add('not-image');
     }
